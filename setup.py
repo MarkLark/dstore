e26f1b933e42f5f0cae37dd3de62df58fd7372c5
MarkLark <mark.pittaway@mlit.net.au>
2017-01-23 13:59:38 +1100

    Updated setup.py classifiers (Now supports Python 3.2-3.6)

--- setup.py
+++ setup.py
@@ -25,12 +25,17 @@ setup(
         'tabulate'
     ],
     classifiers=[
-        'Environment :: Web Environment',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        'Topic :: Database :: Front-Ends'
     ]
 )
