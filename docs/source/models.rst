fabbbbc6a123edcb1491ef6a5713f9e576e777b4
MarkLark <mark.pittaway@mlit.net.au>
2017-01-25 12:10:15 +1100

    Docs: Added inline documentation to Variable types, and updated documentation to use autoclass (to use the doc strings)

--- docs/source/models.rst
+++ docs/source/models.rst
@@ -15,9 +15,6 @@ Variables within a Model
 
 .. autoclass:: Variable
     :members:
-    :inherited-members:
-
-    .. automethod:: __init__
 
 RowID
 -----
@@ -50,243 +47,51 @@ Usage
 
 Number
 ------
-This variable is used to store an integer value.
-
 .. autoclass:: Number
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var, mod
-    var.Number( "year", 1950, mods = [ mod.NotNull() ] )
 
 Boolean
 -------
-This variable is used to store a boolean value.
-
 .. autoclass:: Boolean
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.Boolean( "is_available", False )
 
 Character
 ---------
-This variable is used to store a string of static length.
-
 .. autoclass:: Character
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: length
-
-        The length of the string
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers.
-
-        The mod dstore.mod.Length( length ) is always added to this list
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.Character( "is_available", 4 )
 
 Binary
 ------
-This variable is used to store binary data.
-
 .. autoclass:: Binary
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: length
-
-        The length of the data
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-        The mod dstore.mod.Length( length ) is always added to this list
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.Binary( "data", 32 )
 
 String
 ------
-This variable is used to store a string of variable length.
-
 .. autoclass:: String
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: length
-
-        The maximum length of the string
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-        The mod dstore.mod.Length( length ) is always added to this list
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.String( "username", 32 )
 
 Text
 ----
-This variable is used to store text.
-
 .. autoclass:: Text
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.Text( "description" )
 
 Float
 -----
-This variable is used to store a floating point integer.
-
-.. autoclass:: Text
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var
-    var.Float( "price" )
+.. autoclass:: Float
 
 Enum
 ----
-This variable is used to store a an enum.
-
 .. autoclass:: Enum
-
-    .. attribute:: name
-
-        The name of the variable
-
-    .. attribute:: values
-
-        The list of available choices
-
-    .. attribute:: default
-
-        The default value if none is supplied
-
-    .. attribute:: mods
-
-        A list of Variable Modifiers
-
-        The mod dstore.mod.InEnum( values ) is always added to this list
-
-Usage:
-
-.. code-block:: python
-
-    from dstore import var, mod
-    var.Enum( "car_type", [ "truck", "bus", "ute", "car", "motorbike" ], mods = [ mod.NotNull() ] )
 
 ForeignKey
 ----------
-dstore.var.ForeignKey
+.. autoclass:: ForeignKey
 
 Date
 ----
-dstore.var.Date
+.. autoclass:: Date
 
 Time
 ----
-dstore.var.Time
+.. autoclass:: Time
 
 DateTime
 --------
-dstore.var.DateTime
+.. autoclass:: DateTime
 
 
 Modifiers
--- dstore/var.py
+++ dstore/var.py
@@ -6,19 +6,14 @@ class Variable( object ):
     The base class used for creating Variable types
     This also provides a validate method which iterates over the
     modifiers and runs their validate method on this variable instance.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
     """
     type = None
 
     def __init__( self, name, default = None, mods = None ):
-        """
-        Construct a new Variable type
-
-        :param name: The name of the Variable instance inside a Model Class
-        :param default: The default value to give this variable
-        :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
-
-        This constructs a new Variable instance
-        """
         self.name    = name
         self.default = default
         self._mods   = mods
@@ -38,14 +33,57 @@ class Variable( object ):
 
 
 class Number( Variable ):
+    """
+    This variable type is used to store an integer value.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var, mod
+        var.Number( "year", 1950, mods = [ mod.NotNull() ] )
+    """
     type = "Number"
 
 
 class Boolean( Variable ):
+    """
+    This variable is used to store a boolean value.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Boolean( "is_available", False )
+    """
     type = "Boolean"
 
 
 class Character( Variable ):
+    """
+    This variable is used to store a string of static length.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param length: The length of the string
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers. The mod dstore.mod.Length( length ) is always added to this list
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Character( "is_available", 4 )
+    """
     type = "Character"
 
     def __init__( self, name, length, default = None, mods = None ):
@@ -55,6 +93,21 @@ class Character( Variable ):
 
 
 class Binary( Variable ):
+    """
+    This variable is used to store binary data.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param length: The length of the data
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers. The mod dstore.mod.Length( length ) is always added to this list
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Binary( "data", 32 )
+    """
     type = "Binary"
 
     def __init__( self, name, length, default = None, mods = None ):
@@ -64,6 +117,21 @@ class Binary( Variable ):
 
 
 class String( Variable ):
+    """
+    This variable is used to store a string of variable length.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param length: The length of the string
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers. The mod dstore.mod.Length( length ) is always added to this list
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.String( "username", 32 )
+    """
     type = "String"
 
     def __init__( self, name, length, default = None, mods = None ):
@@ -73,14 +141,57 @@ class String( Variable ):
 
 
 class Text( Variable ):
+    """
+    This variable is used to store text.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Text( "description" )
+    """
     type = "Text"
 
 
 class Float( Variable ):
+    """
+    This variable is used to store a floating point integer.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Float( "price" )
+    """
     type = "Float"
 
 
 class Enum( Variable ):
+    """
+    This variable is used to store a an enum.
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param values: The list of available choices
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers. The mod dstore.mod.InEnum( values ) is always added to this list
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Enum( "car_type", [ "truck", "bus", "ute", "car", "motorbike" ], mods = [ mod.NotNull() ] )
+    """
     type = "Enum"
 
     def __init__( self, name, values, default = None, mods = None ):
@@ -90,6 +201,32 @@ class Enum( Variable ):
 
 
 class ForeignKey( Number ):
+    """
+    This variable is used to link the Model Instance to another Model Instance
+
+    :param namespace: The namespace of the Model Class this variable references
+
+    This variable type is a subclass of dstore.var.Number, with the following mods:
+
+    * dstore.mod.NotNull
+    * dstore.mod.ForeignKey
+
+    The name given to this variable is the name of the Model Class being referenced with a suffix of '_id"
+
+    For example:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.ForeignKey( "cars.make" )
+
+    Is similar to:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Number( "cars_make_id", mods = [ dstore.mod.NotNull(), dstore.mod.ForeignKey( "cars.make" ) )
+    """
     type = "ForeignKey"
 
     def __init__( self, namespace ):
@@ -97,14 +234,56 @@ class ForeignKey( Number ):
 
 
 class Date( Variable ):
+    """
+    This variable is used to store a date value (without a timezone offset).
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Date( "build_date" )
+    """
     type = "Date"
 
 
 class Time( Variable ):
+    """
+    This variable is used to store a time value (without a timezone offset).
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.Time( "build_time" )
+    """
     type = "Time"
 
 
 class DateTime( Variable ):
+    """
+    This variable is used to store a date and time value (without a timezone offset).
+
+    :param name: The name of the Variable instance inside a Model Class
+    :param default: The default value to give this variable
+    :param mods: A list of modifiers, i.e. dstore.mod.NotNull()
+
+    Usage:
+
+    .. code-block:: python
+
+        from dstore import var
+        var.DateTime( "built_on" )
+    """
     type = "DateTime"
 
 RowID = Number( "id", mods = [ mod.AutoIncrement(), mod.PrimaryKey(), mod.Unique() ] )
