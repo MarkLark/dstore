068fca71cd6de7c47164daf7b473408990c7260e
MarkLark <mark.pittaway@mlit.net.au>
2017-01-25 12:11:42 +1100

    Docs: Updated sphinx config file to use ReadTheDocs theme and extensions for generating the documentation locally

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
+# import os
+# on_rtd = os.environ.get('READTHEDOCS') == 'True'
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -31,7 +31,14 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'readthedocs_ext.readthedocs'
+]
+
+suppress_warnings = ['image.nonlocal_uri']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,16 +92,20 @@ todo_include_todos = False
 
 # If the build is being done on ReadTheDocs, set to use their default theme
 # Otherwise use the flask_small theme (including the github ribbon)
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'flask_small'
+# if on_rtd:
+#     html_theme = 'default'
+# else:
+#     html_theme = 'flask_small'
+#
+#     # Theme options are theme-specific and customize the look and feel of a theme
+#     # further.  For a list of options available for each theme, see the
+#     # documentation.
+#     #
+#     html_theme_options = dict( github_fork = 'MarkLark/pyman', index_logo = False )
 
-    # Theme options are theme-specific and customize the look and feel of a theme
-    # further.  For a list of options available for each theme, see the
-    # documentation.
-    #
-    html_theme_options = dict( github_fork = 'MarkLark/pyman', index_logo = False )
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -163,3 +174,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+
